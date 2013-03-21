ed20c17c45eba98e3f1ec0aa2700a0ee56358ab3
Balazs Strenner <strennerb@gmail.com>
2013-03-21 15:29:57 -0500

    Writing the Vector and SquareMatrix classes as container classes of the Matrix class. Not completely finished, will try inheritance instead.

--- Arnoux_Yoccoz/AlmostPFMatrix.cpp
+++ Arnoux_Yoccoz/AlmostPFMatrix.cpp
@@ -10,134 +10,31 @@
 
 
 
-
-
 //--------//
-// Vector //
+// Matrix //
 //--------//
 
-template <typename Type>
-const floating_point_type Vector<Type>::ALLOWED_ERROR = 0.0000000000000001;
-
-
 
 template <typename Type>
-Vector<Type>::Vector(const std::vector<Type>& vector) :
-    m_entries(vector)
-{
-    if (vector.size() == 0) {
-        throw std::runtime_error("The length of a vector (in linear algebra) must be at least 1.");
-    }
-}
-
-
-
-template <typename Type>
-Vector<Type>::Vector(int size) :
-    Vector<Type>(std::vector<Type>(size))
-{
-}
-
-
-
-
-
-template <typename Type>
-template <typename OtherType>
-Vector<Type>::Vector(const Vector<OtherType>& vector) :
-    m_entries(vector.size())
-{
-    for (int i = 0; i < size(); i++) {
-        m_entries[i] = vector.getEntry(i);
-    }
-}
-
-
-
-
-template <typename Type>
-Vector<Type> operator*(const SquareMatrix<Type>& matrix, const Vector<Type>& vector){
-    if (matrix.size() != vector.size()) {
-        throw std::runtime_error("The size of a square matrix and a vector must be the same in order to multiply them.");
-    }
-    const int& size = matrix.size();
-    Vector<Type> product(size);
-    for (int i = 0; i < size; i++) {
-        Type sum = 0;
-        for (int k = 0; k < size; k++) {
-            sum += matrix.getEntry(i, k) * vector.getEntry(k);
-        }
-        product.setEntry(i, sum);
-    }
-    return product;
-}
-
-
-
-template <typename Type>
-Type Vector<Type>::maxEntry() const{
-    Type maxValue = 0;
-    for (int i = 0; i < size(); i++) {
-        if (maxValue < m_entries[i]) {
-            maxValue = m_entries[i];
-        }
-    }
-    return maxValue;
-}
-
-
-
-
-void normalize(Vector<floating_point_type>& vector){
-    floating_point_type maxValue = vector.maxEntry();
-    for (int i = 0; i < vector.size(); i++) {
-        vector.setEntry(i, vector.getEntry(i)/maxValue);
-    }
-}
-
-
-
-template <typename Type>
-bool isCloseEnough(const Vector<Type>& vector1, const Vector<Type>& vector2){
-    assert(vector1.size() == vector2.size());
- 
-    for (int i = 0; i < vector1.size(); i++) {
-        if (fabs(vector1.getEntry(i) - vector2.getEntry(i)) > Vector<Type>::ALLOWED_ERROR) {
-            return false;
-        }
-    }
-    std::cout.precision(20);
-    floating_point_type error = fabs(vector1.getEntry(1) - vector2.getEntry(1));
-    std::cout << " " << (error == 0 ? -1 : error) << " ";
-    return true;
-}
-
-
-
-
-
-
-//--------------//
-// SquareMatrix //
-//--------------//
-
-
-
-template <typename Type>
-SquareMatrix<Type>::SquareMatrix(const std::vector<std::vector<Type>>& entries){
+Matrix<Type>::Matrix(const std::vector<std::vector<Type>>& entries){
     if (entries.empty()) {
-        throw std::runtime_error("The argument for constucting SquareMatrix can't be empty.");
+        throw std::runtime_error("A matrix must have at least one row.");
     }
-    if (entries.size() != entries[0].size()) {
-        throw std::runtime_error("The height must equal the width for a squarematrix.");
+    if (entries[0].empty()) {
+        throw std::runtime_error("The width of a matrix must be at least 1.");
+    }
+    for (int i = 1; i < entries.size(); i++) {
+        if (entries[1].size() != entries[0].size()) {
+            throw std::runtime_error("In a matrix the length of the rows must be equal.");
+        }
     }
     m_entries = entries;
 }
 
 
 template <typename Type>
-SquareMatrix<Type>::SquareMatrix(int size) :
-    SquareMatrix<Type>(std::vector<std::vector<Type>>(size, std::vector<Type>(size)))
+Matrix<Type>::Matrix(int height, int width) :
+    Matrix<Type>(std::vector<std::vector<Type>>(height, std::vector<Type>(width)))
 {
 }
 
@@ -146,12 +43,12 @@ SquareMatrix<Type>::SquareMatrix(int size) :
 
 template <typename Type>
 template <typename OtherType>
-SquareMatrix<Type>::SquareMatrix(const SquareMatrix<OtherType>& squareMatrix) :
-    m_entries(squareMatrix.size(), std::vector<Type>(squareMatrix.size()))
+Matrix<Type>::Matrix(const Matrix<OtherType>& matrix) :
+    m_entries(matrix.height(), std::vector<Type>(matrix.width()))
 {
-    for (int i = 0; i < size(); i++) {
-        for (int j = 0; j < size(); j++) {
-            m_entries[i][j] = squareMatrix.getEntry(i, j);
+    for (int i = 0; i < height(); i++) {
+        for (int j = 0; j < width(); j++) {
+            m_entries[i][j] = matrix.getEntry(i, j);
         }
     }
 }
@@ -162,16 +59,15 @@ SquareMatrix<Type>::SquareMatrix(const SquareMatrix<OtherType>& squareMatrix) :
 
 
 template <typename Type>
-SquareMatrix<Type> operator*(const SquareMatrix<Type>& m1, const SquareMatrix<Type>& m2){
-    if (m1.size() != m2.size()) {
-        throw std::runtime_error("Square matrices of different sizes are not possible to multiply.");
+Matrix<Type> operator*(const Matrix<Type>& m1, const Matrix<Type>& m2){
+    if (m1.width() != m2.height()) {
+        throw std::runtime_error("Dimensions of matrices don't match for multiplication.");
     }
-    const int& size = m1.size();
-    SquareMatrix<Type> product(size);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    Matrix<Type> product(m1.height(), m2.width());
+    for (int i = 0; i < m1.height(); i++) {
+        for (int j = 0; j < m2.width(); j++) {
             Type sum = 0;
-            for (int k = 0; k < size; k++) {
+            for (int k = 0; k < m1.width(); k++) {
                 sum += m1.getEntry(i, k) * m2.getEntry(k, j);
             }
             product.setEntry(i, j, sum);
@@ -184,10 +80,10 @@ SquareMatrix<Type> operator*(const SquareMatrix<Type>& m1, const SquareMatrix<Ty
 
 
 template <typename Type>
-std::ostream& operator<<(std::ostream& Out, const SquareMatrix<Type>& matrix){
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix.Size(); j++) {
-            Out << matrix.m_data[i][j] << " ";
+std::ostream& operator<<(std::ostream& Out, const Matrix<Type>& matrix){
+    for (int i = 0; i < matrix.height(); i++) {
+        for (int j = 0; j < matrix.width(); j++) {
+            Out << matrix.m_entries[i][j] << " ";
         }
         Out << std::endl;
     }
@@ -197,25 +93,25 @@ std::ostream& operator<<(std::ostream& Out, const SquareMatrix<Type>& matrix){
 
 
 template <typename Type>
-SquareMatrix<Type> SquareMatrix<Type>::transpose() const{
-    std::vector<std::vector<Type>> copy(size(), std::vector<Type>(size()));
+Matrix<Type> Matrix<Type>::transpose() const{
+    std::vector<std::vector<Type>> copy(height(), std::vector<Type>(width()));
     
-    for (int i = 0; i < size(); i++) {
-        for (int j = 0; j < size(); j++) {
+    for (int i = 0; i < height(); i++) {
+        for (int j = 0; j < width(); j++) {
             copy[i][j] = m_entries[j][i];
         }
     }
-    return SquareMatrix<Type>(copy);
+    return Matrix<Type>(copy);
 }
 
 
 
 
 template <typename Type>
-Type SquareMatrix<Type>::maxEntry() const{
-    Type maxValue = 0;
-    for (int i = 0; i < size(); i++) {
-        for (int j = 0; j < size(); j++) {
+Type Matrix<Type>::maxEntry() const{
+    Type maxValue = m_entries[0][0];
+    for (int i = 0; i < height(); i++) {
+        for (int j = 0; j < width(); j++) {
             if (maxValue < m_entries[i][j]) {
                 maxValue = m_entries[i][j];
             }
@@ -228,9 +124,12 @@ Type SquareMatrix<Type>::maxEntry() const{
 
 
 template <typename Type>
-Vector<Type> SquareMatrix<Type>::column(int columnIndex) const{
-    Vector<Type> myVector(size());
-    for (int i = 0; i < size(); i++) {
+Vector<Type> Matrix<Type>::column(int columnIndex) const{
+    if (columnIndex < 0 || columnIndex >= width()) {
+        throw std::runtime_error("Invalid column index for Matrix object.");
+    }
+    Vector<Type> myVector(height());
+    for (int i = 0; i < height(); i++) {
         myVector.setEntry(i, getEntry(i, columnIndex));
     }
     return myVector;
@@ -239,6 +138,102 @@ Vector<Type> SquareMatrix<Type>::column(int columnIndex) const{
 
 
 
+template <typename Type>
+bool isCloseEnough(const Matrix<Type>& matrix1, const Matrix<Type>& matrix2){
+    assert(matrix1.width() == matrix1.width());
+    assert(matrix1.height() == matrix1.height());
+
+    for (int i = 0; i < matrix1.height(); i++) {
+        for (int j = 0; j < matrix2.width(); j++) {
+            if (fabs(matrix1.getEntry(i, j) - matrix2.getEntry(i, j)) > ALLOWED_ERROR) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
+void normalize(Matrix<floating_point_type>& matrix){
+    floating_point_type maxValue = matrix.maxEntry();
+    assert(maxValue > 0);
+    for (int i = 0; i < matrix.height(); i++) {
+        for (int j = 0; j < matrix.width(); j++) {
+            matrix.setEntry(i, j, matrix.getEntry(i, j)/maxValue);
+        }
+    }
+}
+
+
+
+
+
+//--------//
+// Vector //
+//--------//
+
+
+
+
+
+
+template <typename Type>
+Vector<Type>::Vector(int size) :
+    Matrix<Type>(size, 1)
+{
+}
+
+
+template <typename Type>
+Vector<Type>::Vector(const Matrix<Type>& matrix) :
+    m_matrix(matrix)
+{
+    if (matrix.width() > 0) {
+        throw std::runtime_error("Can't make a vector from a matrix with width larger than 1.");
+    }
+}
+
+
+
+template <typename Type>
+Vector<Type> operator*(const Matrix<Type>& matrix, const Vector<Type>& vector){
+    if (matrix.width() != vector.size()) {
+        throw std::runtime_error("The width of the matrix and a vector must be the same in order to multiply them.");
+    }
+    return matrix * vector.m_matrix;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------//
+// SquareMatrix //
+//--------------//
+
+
+
+
+template <typename Type>
+const Matrix<Type>& SquareMatrix<Type>::assertSquareMatrix(const Matrix<Type>& matrix){
+    if (matrix.height() != matrix.width()) {
+        throw std::runtime_error("A SquareMatrix object can only be initialized by a square shaped matrix.");
+    }
+    return matrix;
+}
 
 
 
@@ -259,11 +254,15 @@ Vector<Type> SquareMatrix<Type>::column(int columnIndex) const{
 
 
 
-PerronFrobeniusMatrix::PerronFrobeniusMatrix(const SquareMatrix<long>& intSquareMatrix) :
+PerronFrobeniusMatrix::PerronFrobeniusMatrix(const Matrix<long>& intSquareMatrix) :
     m_squareMatrix(intSquareMatrix),
     m_perronFrobEigenvalue(0),  // initializing the Perron-Frobenius eigenvector and eigenvalue by invalid data
     m_perronFrobEigenvector(0)  // since calculation is costly, we only calculate them when the client asks
 {
+    if (m_squareMatrix.width() != m_squareMatrix.height()) {
+        throw std::runtime_error("A non-square matrix can't be Perron-Frobenius.");
+    }
+    
     for (int i = 0; i < size(); i++) {
         for (int j = 0; j < size(); j++) {
             if(getEntry(i, j) < 0){
--- Arnoux_Yoccoz/AlmostPFMatrix.h
+++ Arnoux_Yoccoz/AlmostPFMatrix.h
@@ -16,39 +16,80 @@
 #include "global.h"
 #include <limits>
 
+static const floating_point_type ALLOWED_ERROR = 0.0000000000000001;
+
+/*
 template <typename Type>
 class SquareMatrix;
+*/
+template <typename Type>
+class Vector;
+
+
+template <typename Type>
+class Matrix{
+public:
+    Matrix(int height, int width);
+    Matrix(const std::vector<std::vector<Type>>& entries);
+    
+    template <typename OtherType>
+    Matrix(const Matrix<OtherType>&);
+    
+    int height() const { return static_cast<int>(m_entries.size()); }
+    int width() const { return static_cast<int>(m_entries[0].size()); }
+    Type getEntry(int i, int j) const { return m_entries[i][j]; }
+    void setEntry(int i, int j, const Type& value) { m_entries[i][j] = value; }
+    Type maxEntry() const;
+    Vector<Type> column(int i) const;
+    
+    Matrix<Type> transpose() const;
+    
+    template <typename myType>
+    friend Matrix<myType> operator*(const Matrix<myType>& m1, const Matrix<myType>& m2);
+    
+    template <typename myType>
+    friend Vector<myType> operator*(const Matrix<myType>& matrix, const Vector<myType>& vector);
+    friend std::ostream& operator<<(std::ostream& Out, const Matrix<Type>& matrix);
+
+protected:
+    std::vector<std::vector<Type>> m_entries;
+
+    template <typename myType>
+    friend bool isCloseEnough(const Matrix<myType>& matrix1, const Matrix<myType>& matrix2);
+};
+
+
+void normalize(Matrix<floating_point_type>&);
 
 
 
 template <typename Type>
 class Vector{
 public:
-    Vector(const std::vector<Type>& vector);
     Vector(int size);
+    Vector(const Matrix<Type>& matrix);
     
     template <typename OtherType>
-    Vector(const Vector<OtherType>&);
+    Vector(const Vector<OtherType>& vector) : m_matrix(vector.m_matrix) {}
     
-    std::vector<Type> toStandardVector() const { return m_entries; }
-    int size() const { return static_cast<int>(m_entries.size()); }
-    Type getEntry(int i) const { return m_entries[i]; }
-    void setEntry(int i, const Type& value) { m_entries[i] = value; }
-    Type maxEntry() const;
+    int size() const {return m_matrix.height(); }
+    
+    //std::vector<Type> toStandardVector() const { return m_entries; }
+    Type getEntry(int i) const { return m_matrix.getEntry(i, 0); }
+    void setEntry(int i, const Type& value) { m_matrix.setEntry(i, 0, value); }
+    Type maxEntry() const { return m_matrix.maxEntry(); }
     
     template <typename myType>
-    friend Vector<myType> operator*(const SquareMatrix<myType>& matrix, const Vector<myType>& vector);
+    friend Vector<myType> operator*(const Matrix<myType>& matrix, const Vector<myType>& vector);
+    friend void normalize(Vector<floating_point_type>& vector);
 private:
-    std::vector<Type> m_entries;
-    static const floating_point_type ALLOWED_ERROR;
-    
-    template <typename myType>
-    friend bool isCloseEnough(const Vector<myType>& vector1, const Vector<myType>& vector2);
+    Matrix<Type> m_matrix;
 
 };
 
+void normalize(Vector<floating_point_type>& vector) { normalize(vector.m_matrix); }
 
-void normalize(Vector<floating_point_type>&);
+
 
 
 
@@ -56,19 +97,19 @@ void normalize(Vector<floating_point_type>&);
 template <typename Type>
 class SquareMatrix{
 public:
-    SquareMatrix(int size);
-    SquareMatrix(const std::vector<std::vector<Type>>& entries);
-    
+    SquareMatrix(int size) : m_matrix(size, size) {}
+    SquareMatrix(const Matrix<Type>& matrix) : m_matrix(assertSquareMatrix(matrix)) {}
+
     template <typename OtherType>
     SquareMatrix(const SquareMatrix<OtherType>&);
     
-    int size() const { return static_cast<int>(m_entries.size()); }
-    Type getEntry(int i, int j) const { return m_entries[i][j]; }
-    void setEntry(int i, int j, const Type& value) { m_entries[i][j] = value; }
-    Type maxEntry() const;
-    Vector<Type> column(int i) const;
+    int size() const { return static_cast<int>( m_matrix.heigth()); }
+    Type getEntry(int i, int j) const { return m_matrix.getEntry(i, j); }
+    void setEntry(int i, int j, const Type& value) { m_matrix.setEntry(i, j, Value); }
+    Type maxEntry() const { return m_matrix.maxEntry(); }
+    Vector<Type> column(int i) const { return m_matrix.column(i); }
     
-    SquareMatrix<Type> transpose() const;
+    SquareMatrix<Type> transpose() const { return SquareMatrix<Type>(m_matrix.transpose()); }
 
     template <typename myType>
     friend SquareMatrix<myType> operator*(const SquareMatrix<myType>& m1, const SquareMatrix<myType>& m2);
@@ -78,7 +119,10 @@ public:
     friend std::ostream& operator<<(std::ostream& Out, const SquareMatrix<Type>& matrix);
 
 protected:
-    std::vector<std::vector<Type>> m_entries;
+    Matrix<Type> m_matrix;
+    
+private:
+    const Matrix<Type>& assertSquareMatrix(const Matrix<Type>& matrix);
 };
 
 
@@ -87,8 +131,8 @@ protected:
 class PerronFrobeniusMatrix{
 public:
     PerronFrobeniusMatrix(const std::vector<std::vector<long>>& entries);
-    PerronFrobeniusMatrix(const SquareMatrix<long>& intSquareMatrix);
-    int size() const{ return m_squareMatrix.size(); }
+    PerronFrobeniusMatrix(const Matrix<long>& intSquareMatrix);
+    int size() const{ return m_squareMatrix.height(); }
     long getEntry(int i, int j) const { return m_squareMatrix.getEntry(i, j); }
     PerronFrobeniusMatrix transpose() const { return PerronFrobeniusMatrix(m_squareMatrix.transpose()); }
     bool IsPerronFrobenius() const { return m_perronFrobEigenvector.size() != 0; }
@@ -97,15 +141,16 @@ public:
     
     
 private:
-    SquareMatrix<long> m_squareMatrix;
+    Matrix<long> m_squareMatrix;
     std::vector<floating_point_type> m_perronFrobEigenvector;
     floating_point_type m_perronFrobEigenvalue;
     
-    SquareMatrix<long> powerUp();
+    Matrix<long> powerUp();
     void initEigenData();
     bool isEigenDataDefined() const { return m_perronFrobEigenvalue != 0; }
-    bool isReachableFromEveryVertex(int vertex, const SquareMatrix<long>&) const;
-    bool isEveryVertexReachableFrom(int vertex, const SquareMatrix<long>&) const;
+    bool isReachableFromEveryVertex(int vertex, const Matrix<long>&) const;
+    bool isEveryVertexReachableFrom(int vertex, const Matrix<long>&) const;
+    
 };
 
 floating_point_type arnouxYoccozStretchFactor(int genus);
